e20c821cea20eeb4872c11e0c17ad1f9eebf0bd5
tarong770 <tarong770@gmail.com>
2019-04-07 13:13:16 +0300

    sorting

---
+++ ConsoleApp2/ConsoleApp2/BubbleSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ConsoleApp2
+{
+    class BubbleSort
+    {
+        public static double bTime;
+        public static long bMem;
+        static public void BSort(int[] arr)
+        {
+            Stopwatch stp = new Stopwatch();
+            stp.Start();
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+                for (int j = 0; j < n - i - 1; j++)
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+            stp.Stop();
+            bTime = stp.Elapsed.TotalMilliseconds;
+            bMem = GC.GetTotalMemory(false);
+
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/InsertionSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ConsoleApp2
+{
+    class InsertionSort
+    {
+        public static double bTime;
+        public static long bMem;
+        static public void insertionsort(int[] arr)
+        {
+            Stopwatch stp = new Stopwatch();
+            stp.Start();
+
+            int n = arr.Length;
+            for (int i = 1; i < n; ++i)
+            {
+                int key = arr[i];
+                int j = i - 1;
+
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+
+            }
+            stp.Stop();
+            bTime = stp.Elapsed.TotalMilliseconds;
+            bMem = GC.GetTotalMemory(false);
+        }
+    }
+}
